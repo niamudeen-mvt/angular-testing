e287c2803e61db64387fb5980bd78dca9bc2f64d
niamudeen-mvt <niamudeen.mvteams@gmail.com>
2024-07-18 17:58:26 +0530

    fix - angular material table pagination

---
+++ src/app/service/api.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  constructor(private _http: HttpClient) {}
+
+  public get(url: string) {
+    return this._http.get(url);
+  }
+}
